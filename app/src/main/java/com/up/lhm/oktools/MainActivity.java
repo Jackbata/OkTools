c9b707b7dc519cffe66c32f43771e80abdfa329d
lhmmouse <1245605110@qq.com>
2019-01-30 10:28:26 +0800

    功能测试

--- app/src/main/java/com/up/lhm/oktools/MainActivity.java
+++ app/src/main/java/com/up/lhm/oktools/MainActivity.java
@@ -2,12 +2,49 @@ package com.up.lhm.oktools;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    /**
+     * 打印日志
+     */
+    private Button mBtnLog;
+    /**
+     * 弹窗
+     */
+    private Button mBtnDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setListener();
+    }
+
+    private void initView() {
+        mBtnLog = findViewById(R.id.btn_log);
+        mBtnDialog = findViewById(R.id.btn_dialog);
+    }
+
+    private void setListener() {
+        mBtnLog.setOnClickListener(this);
+        mBtnDialog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_log:
+
+                break;
+            case R.id.btn_dialog:
+                break;
+            default:
+                break;
+        }
     }
 }
